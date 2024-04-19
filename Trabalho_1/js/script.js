c0c4371cd87d076832cdacd0e8a12272758430d8
JeanRauta <j34nwr@gmail.com>
2024-04-18 23:54:04 -0300

    teste

--- Trabalho_1/js/script.js
+++ Trabalho_1/js/script.js
@@ -1,134 +1,164 @@
-let addNote = document.querySelector('#add-note');//Botão de para adicionar nota
-let closeModalView =  document.querySelector('#close-modal'); //fechar janela modal com os detalhes da nota.
-let modal = document.querySelector('#modal'); //Modal para edição das notas
-let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
-let notes = document.querySelector('#notes');//Lista divs com dados das notas
-let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
-let btnCloseModal = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
-
-//+++++++++++++++++++++++++++++++ EVENTOS +++++++++++++++++++++++++++
+let addNote = document.querySelector('#add-note'); // Botão para adicionar nota
+let closeModalView = document.querySelector('#close-modal'); // Botão para fechar janela modal com os detalhes da nota
+let modal = document.querySelector('#modal'); // Modal para edição das notas
+let modalView = document.querySelector('#modal-view'); // Modal para exibição dos detalhes da nota
+let notes = document.querySelector('#notes'); // Lista divs com dados das notas
+let btnSaveNote = document.querySelector("#btn-save-note"); // Ícone para salvar nota
+let btnCloseModal = document.querySelector("#btn-close-note"); // Ícone para fechar modal de edição de nota
 
 addNote.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  modal.style.display='block';
-  notes.style.display='none';
-  addNote.style.display='none';
-})
+    evt.preventDefault();
+    modal.style.display = 'block';
+    addNote.style.display = 'none';
+    notes.style.display = 'none';
+    document.querySelector("#input-id").value = "";
+    document.querySelector("#input-title").value = "";
+    document.querySelector("#input-content").value = "";
+});
 
-btnCloseModal.addEventListener('click', (evt) =>{
-  evt.preventDefault();
-  listNotes();
-  modal.style.display='none';
-  notes.style.display='flex';
-  addNote.style.display='block';
-})
+btnCloseModal.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    listNotes();
+    modal.style.display = 'none';
+    addNote.style.display = 'block';
+    notes.style.display = 'flex';
+});
 
-btnSaveNote.addEventListener('click', (evt) =>{ 
+btnSaveNote.addEventListener('click', (evt) => {
   evt.preventDefault();
+  modal.style.display = 'none'; // Fecha o modal de edição
+  addNote.style.display = 'block'; // Exibe o botão de adicionar nota
+  notes.style.display = 'flex'; // Exibe a lista de notas
+  
   let objNote = {
-    id: document.querySelector("#input-id").value.trim(), 
-    title: document.querySelector("#input-title").value.trim(), 
-    content: document.querySelector("#input-content").value.trim()
+      id: document.querySelector("#input-id").value.trim(),
+      title: document.querySelector("#input-title").value.trim(),
+      content: document.querySelector("#input-content").value.trim()
   };
   saveNote(objNote);
 });
 
 
-
-
-//+++++++++++++++++++++++++++++++ FUNÇÕES +++++++++++++++++++++++++++
-
-function remove_note(idnote){
-
-  listNotes = loadNotes()
-
-  listNotes.forEach((teste, i) => {
-    if (teste.id == idnote) {
-      teste.splice(i);
-    }
-  });
-
-}
-
-const loadNotes = () => {
-  let listNotes = localStorage.getItem('notes');
-  if(!listNotes){
-    listNotes = [];
-  } else {
-    listNotes = JSON.parse(listNotes);
-  }
-  return listNotes;
-};  
+closeModalView.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    modalView.style.display = 'none';
+    addNote.style.display = 'block';
+    notes.style.display = 'flex';
+});
 
 const saveNote = (note) => {
-  let listNotes = loadNotes();
-
-  if(note.id.length < 1){
-    note.id = new Date().getTime();
-    document.querySelector('#input-id').value = note.id;
-    listNotes.push(note);
-  } else {
-    listNotes.forEach((item, i) => {
-      if(item.id == note.id){
-        listNotes[i] = note;
-      }
-    });
-  }
-
-  note.lastTime = new Date().getTime();
-  console.log(listNotes);
-  listNotes = JSON.stringify(listNotes);
-  localStorage.setItem('notes', listNotes); 
+    let listNotes = loadNotes();
+    note.lastTime = new Date().getTime();
+    if (note.id.length < 1) {
+        note.id = new Date().getTime();
+        document.querySelector('#input-id').value = note.id;
+        listNotes.push(note);
+    } else {
+        listNotes.forEach((item, i) => {
+            if (item.id == note.id) {
+                listNotes[i] = note;
+            }
+        });
+    }
+    listNotes = JSON.stringify(listNotes);
+    localStorage.setItem('notes', listNotes);
+    listNotes(); // Atualiza a lista de notas
+    modal.style.display = 'none'; // Fecha o modal de edição
+    addNote.style.display = 'block'; // Exibe o botão de adicionar nota
+    notes.style.display = 'flex'; // Exibe a lista de notas
 };
 
-const showNote = (note) =>{
-  document.querySelector('#title-note').innerHTML = "<h1>"+note.title+"</h1>";
-  document.querySelector('#content-note').innerHTML = "<p>"+note.content+"</p>";
-  document.querySelector('#title-note').innerHTML += "<p>"+new Date (note.lastTime).toLocaleDateString()+"</p>";
-  
-  document.querySelector(".remove_note").addEventListener("click" , function() {
-    remove_note(note.id)
-  })
-
-  modalView.style.display = 'block';
-  notes.style.display='none';
-  addNote.style.display='none';
+const loadNotes = () => {
+    let listNotes = localStorage.getItem('notes');
+    if (!listNotes) {
+        listNotes = [];
+    } else {
+        listNotes = JSON.parse(listNotes);
+    }
+    return listNotes;
 };
-
-closeModalView.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  listNotes();
-  modalView.style.display='none';
-  notes.style.display='flex';
-  addNote.style.display='block';
-})
 
 const listNotes = () => {
-  notes.innerHTML="";
-  let listNotes = loadNotes();
-  listNotes.forEach((item) =>{
-    let divCard = document.createElement('div');
-    divCard.className = 'card';
-    divCard.style.width = '18rem';
-    notes.appendChild(divCard);
-    let divCardBody = document.createElement('div');
-    divCardBody.className = 'card-body';
-    divCard.appendChild(divCardBody);
-    let h1 = document.createElement('h1');
-    h1.innerText = item.title;
-    divCardBody.appendChild(h1);
-    let pContent = document.createElement('p');
-    divCardBody.appendChild(pContent);
-    let pLastTime = document.createElement('p');
-    pLastTime.innerText = new Date (item.lastTime).toLocaleDateString();
-    divCardBody.appendChild(pLastTime);
-    
-    divCard.addEventListener('click',(evt) =>{
-      showNote(item);
+    notes.innerHTML = "";
+    let listNotes = loadNotes();
+    listNotes.forEach((item) => {
+        let divcard = document.createElement('div');
+        divcard.className = 'card';
+        divcard.style.width = '18rem';
+        notes.appendChild(divcard);
+        let divcardBody = document.createElement('div');
+        divcardBody.className = 'card-body';
+        divcard.appendChild(divcardBody);
+        let h1 = document.createElement('h1');
+        h1.innerText = item.title;
+        divcardBody.appendChild(h1);
+        let pContent = document.createElement('p');
+        pContent.innerHTML = item.content;
+        divcardBody.appendChild(pContent);
+        let pLastTime = document.createElement('p');
+        pLastTime.innerText = "Atualizado em:" + new Date(item.lastTime).toLocaleDateString();
+        divcardBody.appendChild(pLastTime);
+        divcard.addEventListener('click', (evt) => {
+            evt.preventDefault();
+            showNote(item);
+        });
     });
-
-  })
 };
 
-listNotes();
+const showNote = (note) => {
+    notes.style.display = 'none';
+    modalView.style.display = 'block';
+    addNote.style.display = 'none';
+    let titleModalView = document.querySelector('#title-note');
+    titleModalView.innerHTML = "";
+    let h1ModalView = document.createElement('h1');
+    h1ModalView.innerText = note.title;
+    titleModalView.appendChild(h1ModalView);
+    let contentModalView = document.querySelector('#content-note');
+    contentModalView.innerHTML = "";
+    let pModalView = document.createElement('p');
+    pModalView.innerText = note.content;
+    contentModalView.appendChild(pModalView);
+    let dateModalView = document.createElement('p');
+    dateModalView.innerText = new Date(note.lastTime).toLocaleDateString();
+    contentModalView.appendChild(dateModalView);
+    document.querySelector("#controls-note").innerHTML = "";
+    let aDelete = document.createElement('a');
+    let i = document.createElement('i');
+    i.innerHTML = 'Remover';
+    i.style.color = 'red';
+    aDelete.appendChild(i);
+    document.querySelector("#controls-note").appendChild(aDelete);
+    let aEdit = document.createElement('a');
+    let iEdit = document.createElement('i');
+    iEdit.innerHTML = 'Editar';
+    iEdit.style.color = 'red';
+    aEdit.appendChild(iEdit);
+    document.querySelector("#controls-note").appendChild(aEdit);
+    aDelete.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        deleteNote(note.id);
+    });
+    aEdit.addEventListener('click', () => {
+        document.querySelector("#input-id").value = note.id;
+        document.querySelector("#input-title").value = note.title;
+        document.querySelector("#input-content").value = note.content;
+        modal.style.display = 'block';
+        addNote.style.display = 'none';
+        notes.style.display = 'none';
+        modalView.style.display = 'none';
+    });
+};
 
+const deleteNote = (id) => {
+    let noteDel = loadNotes();
+    noteDel = noteDel.filter(note => note.id !== id);
+    localStorage.setItem('notes', JSON.stringify(noteDel));
+    listNotes(); // Atualiza a lista de notas
+    if (modalView.style.display === 'block') {
+        modalView.style.display = 'none'; // Fecha o modal de visualização
+        addNote.style.display = 'block'; // Exibe o botão de adicionar nota
+        notes.style.display = 'flex'; // Exibe a lista de notas
+    }
+};
+listNotes();
